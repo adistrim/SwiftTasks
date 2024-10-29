7bceeb86c426b5b1cad869e0c1867ffa8af93c15
Aditya Raj <adistrim.dev@gmail.com>
2024-10-29 23:54:33 +0530

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Aditya Raj on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
